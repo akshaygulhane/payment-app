af802a5a5d5af34282a2b74f934a8c359a3be8fe
Akshay Gulhane <akshay.gulhane@opcito.com>
2019-05-02 12:44:24 +0530

    update code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const CHARGEBEE_API_KEY = process.env.CHARGEBEE_API_KEY || "";
+const PORT = process.env.PORT || 3000;
 
 var chargebee = require('chargebee');
 
@@ -9,3 +10,7 @@ chargebee.configure({
     site: "https://opcito-dev-test.chargebee.com",
     api_key: CHARGEBEE_API_KEY
 });
+
+app.listen(PORT, () => {
+    console.log(`Server started succesfully on PORT: ${PORT}`);
+})
